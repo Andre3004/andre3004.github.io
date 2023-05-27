86ce4527a87e9110b9a135c1d7d23482eb176394
André Luiz <andre.ferreira@simplesyntax.io>
2023-05-27 13:09:21 -0300

    add pdf route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-// import './App.css';
-import { Header } from './components/Header';
-import { ExperienceSummary } from './components/ExperienceSummary';
-import { RecentProjects } from './components/RecentProjects';
-import { WorkExperience } from './components/WorkExperience';
-import { Skills } from './components/Skills';
-import { Education } from './components/Education';
-import { Languages } from './components/Languages';
+import { Home } from './pages/Home';
+import { useRoutes } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-      <article id="main" className="resume-wrapper text-center position-relative">
-        <div className="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
-          <Header />
-          <div className="resume-body p-5">
-            <ExperienceSummary />
-            <RecentProjects />
-            <div className="row">
-              <WorkExperience />
-              <div className="col-lg-3">
-                <Skills />
-                <Education />
-                <Languages />
-              </div>
-            </div>
-          </div>
-        </div>
-      </article>
-    </>
-  );
-}
+const App = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/pdf', element: <Home /> },
+  ]);
+
+  return routes;
+};
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+// import './App.css';
+import { Header } from '../components/Header';
+import { ExperienceSummary } from '../components/ExperienceSummary';
+import { RecentProjects } from '../components/RecentProjects';
+import { WorkExperience } from '../components/WorkExperience';
+import { Skills } from '../components/Skills';
+import { Education } from '../components/Education';
+import { Languages } from '../components/Languages';
+import { useLocation } from 'react-router-dom';
+
+export const Home = () => {
+  const location = useLocation();
+  console.log();
+
+  const isPdf = () => location.pathname.includes('pdf');
+
+  return (
+    <>
+      <article id="main" className={`${!isPdf() ? 'resume-wrapper-pdf' : ''} text-center position-relative`}>
+        <div className={`${!isPdf() ? 'resume-wrapper-inner-pdf' : ''}  mx-auto text-start bg-white shadow-lg`}>
+          <Header />
+          <div className="resume-body p-5">
+            <ExperienceSummary />
+            <RecentProjects />
+            <div className="row">
+              <WorkExperience />
+              <div className="col-lg-3">
+                <Skills />
+                <Education />
+                <Languages />
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+};
