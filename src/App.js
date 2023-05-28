d7d26445f8664ff59c4cd5eee2943dd11d5e0e99
André Luiz <andre.ferreira@simplesyntax.io>
2023-05-28 11:51:18 -0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Home } from './pages/Home';
-import { useRoutes } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 
 const App = () => {
-  const routes = useRoutes([
+  const router = createHashRouter([
     { path: '/', element: <Home /> },
     { path: '/pdf', element: <Home /> },
   ]);
 
-  return routes;
+  return ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
 };
 
 export default App;
